85b15855b38e9f67163bba8a2b90b9671825407d
Rainer Flores <rainerneno@gmail.com>
2023-04-24 21:06:08 +0800

    Delete config.py

--- config.py
+++
@@ -1,8 +0,0 @@
-dsn_driver = "{IBM DB2 ODBC DRIVER}"
-dsn_database = "bludb"
-dsn_hostname = "0c77d6f2-5da9-48a9-81f8-86b520b87518.bs2io90l08kqb1od8lcg.databases.appdomain.cloud"
-dsn_port = "31198"
-dsn_protocol = "TCPIP"
-dsn_uid = "fcx48934"
-dsn_pwd = "GZ64qzjkYYMeLL9j"
-dsn_security = "SSL"
